5119cd5e48d0615d7579ea974240bc8d29718116
alissonteles <alisson_teles_ribeio@outlook.com>
2024-12-05 08:17:49 -0300

    acabei

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let nomeHeroi = "Alisson"
-let xp = 1400
+let xp = 90700
 
 if (xp < 1000) {
     console.log(nomeHeroi + " é Ferro")
-}else if (1001 <= xp <= 2000) {
+}else if (xp >= 1001 && xp <= 2000) {
     console.log(nomeHeroi + " é Bronze")
+}else if (xp >= 2001 && xp <= 5000) {
+    console.log(nomeHeroi + " é Prata")
+}else if (xp >= 6001 && xp <= 7000) {
+    console.log(nomeHeroi + " é Ouro")
+}else if (xp >= 7001 && xp <= 8000) {
+    console.log(nomeHeroi + " é Platina")
+}else if (xp >= 8001 && xp <= 9000) {
+    console.log(nomeHeroi + " é Ascendente")
+}else if (xp >= 9001 && xp <= 10000) {
+    console.log(nomeHeroi + " é Imortal")
+}else if (xp >= 10001) {
+    console.log(nomeHeroi + " é Radiante")
 }
