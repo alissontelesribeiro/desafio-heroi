f87ac0f7239ba5e206a179a3ee00da6e71a16e1e
alissonteles <alisson_teles_ribeio@outlook.com>
2024-12-05 08:29:56 -0300

    agora sim acabei

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ let nomeHeroi = "Alisson"
 let xp = 90700
 
 if (xp < 1000) {
-    console.log(nomeHeroi + " é Ferro")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
 }else if (xp >= 1001 && xp <= 2000) {
-    console.log(nomeHeroi + " é Bronze")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
 }else if (xp >= 2001 && xp <= 5000) {
-    console.log(nomeHeroi + " é Prata")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
 }else if (xp >= 6001 && xp <= 7000) {
-    console.log(nomeHeroi + " é Ouro")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
 }else if (xp >= 7001 && xp <= 8000) {
-    console.log(nomeHeroi + " é Platina")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
 }else if (xp >= 8001 && xp <= 9000) {
-    console.log(nomeHeroi + " é Ascendente")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
 }else if (xp >= 9001 && xp <= 10000) {
-    console.log(nomeHeroi + " é Imortal")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
 }else if (xp >= 10001) {
-    console.log(nomeHeroi + " é Radiante")
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
 }
