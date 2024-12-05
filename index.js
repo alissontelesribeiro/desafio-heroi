3dcbac4c7aeaccdedb7fd25896b8a92c4c78651e
alissonteles <alisson_teles_ribeio@outlook.com>
2024-12-05 07:35:40 -0300

    Segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Olá mundo")
+let nomeHeroi = "Alisson"
+let xp = 1400
+
+if (xp < 1000) {
+    console.log(nomeHeroi + " é Ferro")
+}else if (1001 <= xp <= 2000) {
+    console.log(nomeHeroi + " é Bronze")
+}
